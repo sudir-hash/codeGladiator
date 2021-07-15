e9b0ddde48f1863ee6fd46f9b97b0f53c8803840
Sudhir <81602162+sudir-hash@users.noreply.github.com>
2021-07-15 14:23:56 +0530

    Update PUSHPOP.c

--- codeGladiator/PUSHPOP.c
+++ codeGladiator/PUSHPOP.c
@@ -3,7 +3,7 @@
 /* A push and pop function for a stack of integers                    */
 /*--------------------------------------------------------------------*/
 #include <stdlib.h>
-#include "CALC.h"
+#include "CALC.h" // included CALC.h header file
 /*--------------------------------------------------------------------*/
 /* input:  stk - stack of integers                                    */
 /*         num - value to push on the stack                           */
@@ -13,9 +13,9 @@ extern void push(IntStack *stk, int num)
 {
   IntLink *ptr;
   ptr = (struct int_link *)malloc(sizeof(IntLink)); /*  PUSHPOP1   */
-  ptr->i = num;                                     /*  PUSHPOP2  statement */
-  ptr->next = stk->top;
-  stk->top = ptr;
+  ptr->i = num;            //changed -> to ->                         /*  PUSHPOP2  statement */
+  ptr->next = stk->top;    //changed -> to ->
+  stk->top = ptr;           //changed -> to ->
 }
 /*--------------------------------------------------------------------*/
 /* return: int value popped from stack                                */
@@ -25,9 +25,9 @@ extern int pop(IntStack *stk)
 {
   IntLink *ptr;
   int num;
-  ptr = stk->top;
-  num = ptr->i;
-  stk->top = ptr->next;
+  ptr = stk->top;       //changed -> to ->
+  num = ptr->i;         //changed -> to ->
+  stk->top = ptr->next;   //changed -> to ->
   free(ptr);
   return num;
 }
